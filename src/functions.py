31f34a234a8fed353254343bbb629be6dcae1c0e
Notaro <davide.notaro@csi-spa.com>
2025-06-28 11:57:29 +0200

    added error handiling and test recognition

--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,7 @@
-def removeCharacters(genericString):
+from colorama import init, Fore, Style
+import os
+
+def removeCharacters(genericString: str) -> str:
     invalidCharacters = ['?', '�', '[',']', '⁻', chr(8314)]
 
     cleanString = genericString
@@ -7,3 +10,17 @@ def removeCharacters(genericString):
         cleanString = cleanString.replace(characters, '')
 
     return cleanString
+
+def testCheck(test: str) -> bool:
+    testFolder = os.path.dirname(test)
+    specPath = test.replace(".txt", ".spec")
+    return os.path.exists(specPath)
+
+def decorateSentence(sentence: str, isRed: bool):
+    init()
+    if isRed:
+        print(Fore.RED)
+    print("---------------------------------------------------------------------------------")
+    print(sentence)
+    print("---------------------------------------------------------------------------------")
+    print(Style.RESET_ALL)
--- src/main.py
+++ src/main.py
@@ -20,7 +20,7 @@ cleanFolderName = cleanFolderName + "_manipulated"
 print("Copying files...")
 manipulatedFolder = os.path.join(parentFolder,cleanFolderName)
 shutil.copytree(sourceFolder, manipulatedFolder, dirs_exist_ok=True)
-print("All files have been copied.")
+print("All files have been copied.\n")
 
 for root, dirs, files in os.walk(manipulatedFolder, topdown=False):
     for file in files:
@@ -34,6 +34,36 @@ for root, dirs, files in os.walk(manipulatedFolder, topdown=False):
         fullRenamePath = os.path.join(root, newDirName)
 
         os.rename(fullPath, fullRenamePath)
+
+txtFiles: list[str] = [] 
+for root, dirs, files in os.walk(manipulatedFolder, topdown=False):
+    for file in files:
+        if file.endswith(".txt"):
+            fullPath = os.path.join(root, file)
+            txtFiles.append(fullPath)
+
+nTests = len(txtFiles)
+failedFiles = []
+
+# Start of the main loop
+for test in txtFiles:
+    folderTest = os.path.dirname(test)
+    relativePath = test.replace(manipulatedFolder, '')
+
+
+    # Check if the txt file is a test
+    if not functions.testCheck(test):
+        print(relativePath + " was not a test.")
+        os.remove(test)
+        continue
+
+    try:
+        raise ValueError('A very specific bad thing happened.')
+    except Exception as e:
+        failedFiles.append(relativePath)
+        errorMessage = "There was an error: " + str(e) + "\n"
+        errorMessage = errorMessage + relativePath + " was NOT processed"
+        functions.decorateSentence(errorMessage, True)
 
 
 
